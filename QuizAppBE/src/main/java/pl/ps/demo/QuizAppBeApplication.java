466052ae68194c00a5a71de50a3548e35d509013
pioste <pioste97@gmail.com>
2021-10-19 23:19:25 +0200

    Initial commit

---
+++ QuizAppBE/src/main/java/pl/ps/demo/QuizAppBeApplication.java
@@ -0,0 +1,13 @@
+package pl.ps.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAppBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizAppBeApplication.class, args);
+    }
+
+}
---
+++ QuizAppBE/src/test/java/pl/ps/demo/QuizAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ps.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
